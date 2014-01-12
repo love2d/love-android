78cb9c5539da895b889f6f32c5248ab4e85b4712
Martin Felis <martin@fysx.org>
2014-01-12 21:32:40 +0100

    alpha6 using newest Löve GLES code

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,7 @@
-
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 # APP_STL := stlport_static 
 APP_STL := gnustl_shared
 APP_ABI := armeabi-v7a
+APP_CPPFLAGS := -frtti
+# APP_OPTIM := debug
