35f95a0c5894b832e769351fa7f52a61cbd20b19
fysx <martin@fysx.org>
2014-01-27 23:11:42 +0100

    new build script for LuaJIT, adapted from moai-dev (thanks to Jairo Luiz)

--- jni/LuaJIT-2.0.1/Android.mk
+++ jni/LuaJIT-2.0.1/Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libluajit
-LOCAL_SRC_FILES := src/libluajit.a
+LOCAL_SRC_FILES := android/$(TARGET_ARCH_ABI)/libluajit.a
 
 include $(PREBUILT_STATIC_LIBRARY)
