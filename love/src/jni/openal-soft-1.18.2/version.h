f40577c9cbf01d640c0a8b77813c95ac477a8a4a
Martin Felis <martin@fysx.org>
2018-02-01 15:53:05 +0100

    Upgraded OpenAL-Soft to 0.18.2

---
+++ love/src/jni/openal-soft-1.18.2/version.h
@@ -0,0 +1,8 @@
+/* Define to the library version */
+#define ALSOFT_VERSION "1.18.2"
+
+/* Define the branch being built */
+#define ALSOFT_GIT_BRANCH "HEAD"
+
+/* Define the hash of the head commit */
+#define ALSOFT_GIT_COMMIT_HASH "ce60760"
