3b7407ab34070f39a1e029c3e9714d4af394b233
Tae Hanazono <auahdark687291@gmail.com>
2019-02-08 20:20:37 +0800

    Fix onResume using unsupported flags in some older devices.

--- love/src/main/java/org/love2d/android/GameActivity.java
+++ love/src/main/java/org/love2d/android/GameActivity.java
@@ -171,13 +171,19 @@ public class GameActivity extends SDLActivity {
         super.onResume();
 
         if (immersiveActive) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            int flags = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+            if (android.os.Build.VERSION.SDK_INT >= 16) {
+                flags |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                         View.SYSTEM_UI_FLAG_FULLSCREEN;
+            }
+            if (android.os.Build.VERSION.SDK_INT >= 19) {
+                flags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            }
+
+            getWindow().getDecorView().setSystemUiVisibility(flags);
         }
     }
 
