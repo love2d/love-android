8186794a03a3b9cbda0a4d737db6ce72caadddaf
Miku AuahDark <auahdark687291@gmail.com>
2019-08-10 16:27:41 +0800

    Remove SDL_android_main.c from LOVE Android.mk
    A notice from SDL_android_main.c:
    "As of SDL 2.0.6 this file is no longer necessary."

--- love/src/jni/love/Android.mk
+++ love/src/jni/love/Android.mk
@@ -107,8 +107,6 @@ LOCAL_STATIC_LIBRARIES := libvorbis libogg libtheora libmodplug libfreetype libl
 # $(info liblove: include dirs $(LOCAL_C_INCLUDES))
 # $(info liblove: src files $(LOCAL_SRC_FILES))
 
-SDL_PATH := ../SDL2-2.0.9
-LOCAL_SRC_FILES += $(SDL_PATH)/src/main/android/SDL_android_main.c 
 LOCAL_LDLIBS := -lz -lGLESv1_CM -lGLESv2 -ldl -landroid
 LOCAL_LDFLAGS := -Wl,--allow-multiple-definition
 
